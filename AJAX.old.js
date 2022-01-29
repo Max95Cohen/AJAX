86010ead7c4a9df261624f20933d505c00ec8c25
Max Cohen <showman_sexy@bk.ru>
2022-01-29 22:49:32 +0600

    update ajax.old.js

--- AJAX.old.js
+++ AJAX.old.js
@@ -22,9 +22,9 @@ function AJAX_(method_, URL_, data_, success_, fail_) {
 
     x.open(method_, URL_, true);
 
-    if (method_.toLowerCase() === 'post') {
-        x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 
+    if (method_.toLowerCase() === 'post') {
         if (!isFile_) {
             x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         }
